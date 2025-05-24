3885c1a7d7198df0468bf454c06a9111e73824b1
tqluc205 <luctqps38844@gmail.com>
2025-05-24 22:10:34 +0700

    Home and Profile

--- src/theme/assets.ts
+++ src/theme/assets.ts
@@ -1,3 +1,5 @@
+import { HomeScreen } from "../screens/main/HomeScreen";
+
 export const assets = {
   icons: {
     bottomTab: {
@@ -21,9 +23,33 @@ export const assets = {
         active: require('../../assets/icons/profile-active.png'),
         inactive: require('../../assets/icons/profile-inactive.png'),
       },
+      
     },
-  },
-  images: {
+    homeScreen: {
+       bell: require('../../assets/icons/bell.png'),
+       cart: require('../../assets/icons/cart.png'),
+       search: require('../../assets/icons/Search.png'),
+       star: require('../../assets/icons/star.png'),
+       plus: require('../../assets/icons/plus.png'),
+       scissors: require('../../assets/icons/scissors.png'),
+       house: require('../../assets/icons/house.png')    
+      },
+      profileScreen: {
+        user: require('../../assets/icons/user.png'),
+        adderss: require('../../assets/icons/adderss.png'),
+        card: require('../../assets/icons/card.png'),
+        favourite: require('../../assets/icons/favourite.png'),
+        notification: require('../../assets/icons/notification.png'),
+        fa: require('../../assets/icons/fa.png'),
+        setting: require('../../assets/icons/setting.png'),
+        right: require('../../assets/icons/right.png'),
+        back: require('../../assets/icons/back.png')
 
+      }
+  },
+
+  images: {
+    logo: require('../../assets/icons/logo.png'),
+    image: require('../../assets/icons/image 1.png')
   },
 };
